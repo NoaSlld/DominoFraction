3e2579e97d5db3f153519a8ac888214c1ce48af8
Alicia <alicia@MacBook-Pro-de-alicia.local>
2023-02-28 10:59:47 +0100

    ajoute la generation

--- js.js
+++ js.js
@@ -16,31 +16,21 @@ function charger() {
         return;
     });
     //document.querySelector(".droite").addEventListener("drop",()=>{});
-    dominos.push({
-        val1: null,
-        val2: (Math.floor(Math.random() * 10 ) + 1) + "/" + (Math.floor(Math.random() * 10 ) + 1) + " * " + (Math.floor(Math.random() * 10 ) + 1)  + "/" + (Math.floor(Math.random() * 10 ) + 1),
-        psi: 0
-      });
-    for (let i = 0 ; i < 27 ; i++){
-      createAndPushOneDomino(); 
+   
+    generatefirst7dominos()
+    for( i ; i <= 21;){
+        createAndPushOneDomino()
     }
-    
-    let [fraction1, fraction2] = dominos[27].val2.split('*');
-    let [numerator1, denominator1] = fraction1.split('/');
-    let [numerator2, denominator2] = fraction2.split('/');
-    
-    prodNum = numerator1 * numerator2
-    prodDenum = denominator1 * denominator2
-    
-    dominos[0].val1 = reduce(prodNum,prodDenum)[0] + "/" + reduce(prodNum,prodDenum)[1]
-    
     Distribuerpieces();
     console.log(dominos);
 }
 document.addEventListener("DOMContentLoaded", charger);
 
 
-var i = 0;
+var i = 1;
+var aux = 0;
+var nb = 5;
+
 function reduce(number,denomin){
     var gcd = function gcd(a,b){
         return b ? gcd(b, a%b) : a;
@@ -48,32 +38,127 @@ function reduce(number,denomin){
     gcd = gcd(number,denomin);
     return [number/gcd, denomin/gcd];
 }
+
+function createDomino(nb){
+    let [fraction1, fraction2] = dominos[nb].val2.split('*');
+    let [numerator1, denominator1] = fraction1.split('/');
+    let [numerator2, denominator2] = fraction2.split('/');
+    prodNum = numerator1 * numerator2
+    prodDenum = denominator1 * denominator2
+    return reduce(prodNum,prodDenum)[0] + "/" + reduce(prodNum,prodDenum)[1]
+}
+
 function createAndPushOneDomino(){
-  firstNum = Math.floor(Math.random() * 10 ) + 1;
-  firstDenum = Math.floor(Math.random() * 10 ) + 1;
-
-  secondNum = Math.floor(Math.random() * 10 ) + 1;
-  secondDenum = Math.floor(Math.random() * 10 ) + 1;
+  g = Math.floor(Math.random() * 5 ) + 1;
+  h = Math.floor(Math.random() * 5 ) + 1;
   
-
-  let [fraction1, fraction2] = dominos[i].val2.split('*');
-  let [numerator1, denominator1] = fraction1.split('/');
-  let [numerator2, denominator2] = fraction2.split('/');
-
-  prodNum = numerator1 * numerator2
-  prodDenum = denominator1 * denominator2
-  
-  domino = {
-    val1: reduce(prodNum,prodDenum)[0] + "/" + reduce(prodNum,prodDenum)[1],
-    val2: firstNum + "/" + firstDenum + " * " + secondNum + "/" + secondDenum,
-    psi: 0
+  if (i <= 6) {
+    let [fraction1, fraction2] = dominos[aux].val2.split('*');
+    let [numerator1, denominator1] = fraction1.split('/');
+    let [numerator2, denominator2] = fraction2.split('/');
+    dominos.push({
+        val1: createDomino(nb),
+        val2: reduce(numerator1*h,denominator1)[0] + "/" + reduce(numerator1*h,denominator1)[1] + " * " + reduce(numerator2,denominator2*h)[0] + "/" + reduce(numerator2,denominator2*h)[1]
+    }); 
   }
+  if (i > 6 && i <= 11) {
+    if(i == 7) {
+        aux = 0;
+        nb = 4;
+    }
+    let [fraction1, fraction2] = dominos[aux].val2.split('*');
+    let [numerator1, denominator1] = fraction1.split('/');
+    let [numerator2, denominator2] = fraction2.split('/');
+    dominos.push({
+        val1: createDomino(nb),
+        val2: reduce(numerator1*g,denominator1*h)[0] + "/" + reduce(numerator1*g,denominator1*h)[1] + " * " + reduce(numerator2*h,denominator2*g)[0] + "/" + reduce(numerator2*h,denominator2*g)[1]
+    }); 
+   }
 
+   if (i > 11 && i <= 15) {
+    if(i == 12) {
+        aux = 0;
+        nb = 3;
+    }
+    let [fraction1, fraction2] = dominos[aux].val2.split('*');
+    let [numerator1, denominator1] = fraction1.split('/');
+    let [numerator2, denominator2] = fraction2.split('/');
+    dominos.push({
+        val1: createDomino(nb),
+        val2: reduce(numerator1*g,denominator1)[0] + "/" + reduce(numerator1*g,denominator1)[1] + " * " + reduce(numerator2,denominator2*g)[0] + "/" + reduce(numerator2,denominator2*g)[1]
+    }); 
+   }
 
-  if(verifIfDominoExist(domino,dominos) == false){
-    dominos.push(domino);
-  }
+   if (i > 15 && i <= 18) {
+    if(i == 16) {
+        aux = 0;
+        nb = 2;
+    }
+    let [fraction1, fraction2] = dominos[aux].val2.split('*');
+    let [numerator1, denominator1] = fraction1.split('/');
+    let [numerator2, denominator2] = fraction2.split('/');
+    dominos.push({
+        val1: createDomino(nb),
+        val2: reduce(numerator1*h,denominator1*g)[0] + "/" + reduce(numerator1*h,denominator1*g)[1] + " * " + reduce(numerator2*g,denominator2*h)[0] + "/" + reduce(numerator2*g,denominator2*h)[1]
+    }); 
+   }
+   if (i > 18 && i <= 20) {
+    if(i == 19) {
+        aux = 0;
+        nb = 1;
+    }
+    let [fraction1, fraction2] = dominos[aux].val2.split('*');
+    let [numerator1, denominator1] = fraction1.split('/');
+    let [numerator2, denominator2] = fraction2.split('/');
+    dominos.push({
+        val1: createDomino(nb),
+        val2: reduce(numerator1*h,denominator1*g)[0] + "/" + reduce(numerator1*h,denominator1*g)[1] + " * " + reduce(numerator2*g,denominator2*h)[0] + "/" + reduce(numerator2*g,denominator2*h)[1]
+    }); 
+   }
+   if (i > 20 && i <= 21) {
+    if(i == 21) {
+        aux = 0;
+        nb = 0;
+    }
+    let [fraction1, fraction2] = dominos[aux].val2.split('*');
+    let [numerator1, denominator1] = fraction1.split('/');
+    let [numerator2, denominator2] = fraction2.split('/');
+    dominos.push({
+        val1: createDomino(nb),
+        val2: reduce(numerator1*h,denominator1*g)[0] + "/" + reduce(numerator1*h,denominator1*g)[1] + " * " + reduce(numerator2*g,denominator2*h)[0] + "/" + reduce(numerator2*g,denominator2*h)[1]
+    }); 
+   }
+
+  aux++
   i++;
+  }
+
+
+function generatefirst7dominos(){
+    sixNum = Math.floor(Math.random() * 10 ) + 1;
+    sixDenum = Math.floor(Math.random() * 10 ) + 1;
+    if (sixDenum ==  sixNum || sixDenum == 1 ){
+        generatefirst7dominos();
+        dominos = [];
+    } 
+    sixNumBis = Math.floor(Math.random() * 10 ) + 1;
+    if ((sixDenumBis = Math.floor(Math.random() * 10 ) + 1) ==  sixNumBis || sixDenumBis == 1 ){
+        generatefirst7dominos();
+        dominos = [];
+    } 
+    prodNum = sixNum * sixNumBis
+    prodDenum = sixDenum * sixDenumBis
+  
+    for (let aux = 0 ; aux < 6 ; aux++){
+        dominos.push({
+            val1: reduce(prodNum,prodDenum)[0] + "/" + reduce(prodNum,prodDenum)[1],
+            val2: (Math.floor(Math.random() * 10 ) + 1) + "/" + (Math.floor(Math.random() * 10 ) + 1) + " * " + (Math.floor(Math.random() * 10 ) + 1)  + "/" + (Math.floor(Math.random() * 10 ) + 1) 
+        });
+    }
+    dominos.push({
+        val1: reduce(prodNum,prodDenum)[0] + "/" + reduce(prodNum,prodDenum)[1],
+        val2: sixNum + "/" + sixDenum + " * " + sixNumBis + "/" + sixDenumBis
+    });
 }
 
 function verifIfDominoExist(dominoToAdd, dominos){
@@ -84,6 +169,7 @@ function verifIfDominoExist(dominoToAdd, dominos){
   })
   return false;
 }
+
 
 
 
